8b546960a72ac50fcd5468ede12448f3665c361d
redfrost0 <27699882+redfrost0@users.noreply.github.com>
2024-08-10 18:50:52 +0100

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -3,14 +3,6 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:8440/:path*' // Proxy to Backend
-      }
-    ]
-  },
   output: "standalone",
 };
 
