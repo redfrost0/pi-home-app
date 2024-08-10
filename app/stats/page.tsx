bf28828dbc3c5fd4139229614dccbec55400290c
redfrost0 <27699882+redfrost0@users.noreply.github.com>
2024-08-10 22:10:27 +0100

    add loading

--- app/stats/page.tsx
+++ app/stats/page.tsx
@@ -2,11 +2,19 @@
 import useSWR from 'swr'
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Progress } from "@/components/ui/progress";
+import { LoadingSpinner } from '@/components/ui/loading';
 const fetcher = (url: string | URL | Request) => fetch(url).then(r => r.json())
 
 export default function Home() {
-  const { data, error } = useSWR('http://localhost:8440/stats', fetcher, { refreshInterval: 2000 })
-  if (data) return (
+  const { data, isLoading, error } = useSWR('api/stats', fetcher, { refreshInterval: 2000 })
+  if (isLoading) {
+    return (
+      <main className="min-h-screen bg-slate-75 flex flex-col items-center justify-center p-6">
+        <LoadingSpinner />
+        </main>
+  )
+  }
+  return (
     <main className="min-h-screen bg-slate-75 flex flex-col items-center justify-center p-6">
       <h1 className="text-3xl font-bold mb-6 text-foreground">Raspberry Pi</h1>
 
--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,14 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://sys-api.joelspi.org/:path*' // Proxy to Backend
+      }
+    ]
+  },
   output: "standalone",
 };
 
