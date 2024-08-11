f73c5c41430c9d945d706df28fe59998e9f5de49
redfrost0 <27699882+redfrost0@users.noreply.github.com>
2024-08-11 15:08:04 +0100

    add debug button for headers

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,35 @@
+'use client'
+import { Button } from '@/components/ui/button';
 import Link from 'next/link'
+import { buttonVariants } from "@/components/ui/button"
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 
 export default function Home() {
-  return (
-    <main className="min-h-screen bg-background flex flex-col items-center justify-center p-6">
-      <h1 className="text-3xl font-bold mb-6 text-foreground">Raspberry Pi</h1>
-      <Link href="/stats">Stats</Link>
-    </main>
-  );
+  
+
+    return (
+      <main className="min-h-screen bg-background flex flex-col items-center justify-center p-6">
+        <Card className="w-full max-w-md">
+        <CardHeader>
+          <CardTitle>Rasberry Pi</CardTitle>
+          </CardHeader>
+          <CardContent className="space-y-4">
+          <div className="flex justify-around text-sm text-muted-foreground">
+          <Link className={buttonVariants({ variant: "default" })} href="/stats">Stats</Link>
+          <Button onClick={callAPI}>Check Headers</Button>
+            </div>
+            </CardContent>
+        </Card>
+      </main>
+    );
+  }
+
+  const callAPI = async () => {
+    try {
+      const res = await fetch(`https://header-test.joelspi.org/listHeaders`);
+      const data = await res.json();
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
 }
